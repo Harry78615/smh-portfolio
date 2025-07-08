640adccf30d01e67f837bc796947a8e2eb879857
Harris Shah <116918166+Harry78615@users.noreply.github.com>
2025-07-08 23:07:01 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster.tsx";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/Home";
-import NotFound from "@/pages/not-found";
+import { TooltipProvider } from "./components/ui/tooltip";
+import Home from "./pages/Home";
+import NotFound from "./pages/not-found";
 
 function Router() {
   return (
